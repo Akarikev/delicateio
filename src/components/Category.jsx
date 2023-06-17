f786ba46badcf8883fe8c8bb74230286d87034ce
Akarikev <princeelorm17@gmail.com>
2023-06-16 17:23:53 -0700

    new components edited

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,22 +6,22 @@ import { NavLink } from "react-router-dom";
 function Category() {
   return (
     <List>
-      <NavLink to={`/cuisine/Italian`}>
+      <Slink to={`/cuisine/Italian`}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </NavLink>
-      <NavLink to={`/cuisine/American`}>
+      </Slink>
+      <Slink to={`/cuisine/American`}>
         <FaHamburger />
         <h4>American</h4>
-      </NavLink>
-      <NavLink to={`/cuisine/Thai`}>
+      </Slink>
+      <Slink to={`/cuisine/Thai`}>
         <GiNoodles />
         <h4>Thai</h4>
-      </NavLink>
-      <NavLink to={`/cuisine/Japanese`}>
+      </Slink>
+      <Slink to={`/cuisine/Japanese`}>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </NavLink>
+      </Slink>
     </List>
   );
 }
@@ -31,4 +31,36 @@ const List = styled.div`
   justify-content: center;
   margin: 2rem, 0rem;
 `;
+
+const Slink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  width: 6rem;
+  height: 6rem;
+  cursor: pointer;
+  transform: scale(0.8);
+
+  h4 {
+    padding-top: 0.5rem;
+    color: white;
+    font-size: 0.8rem;
+    font-family: Inter;
+  }
+
+  svg {
+    color: white;
+    font-size: 1.5rem;
+  }
+
+  &.active {
+    background: linear-gradient(to right, #f27121, #e94056);
+  }
+`;
+
 export default Category;
--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -4,13 +4,8 @@ import { Link, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 function Cuisine() {
-  const [cuisine, setCuisine] = useState([]);
+  const [Cuisine, setCuisine] = useState([]);
   let params = useParams();
-
-  useEffect(() => {
-    getCuisine(params.type);
-    console.log(params.type);
-  }, [params.type]);
 
   const getCuisine = async (name) => {
     const data = await fetch(
@@ -20,17 +15,46 @@ function Cuisine() {
     );
 
     const recipe = await data.json();
-    setCuisine(recipe.result);
+    setCuisine(recipe.results);
   };
+
+  useEffect(() => {
+    getCuisine(params.type);
+    console.log(params.type);
+  }, [params.type]);
+
   return (
-    <div>
-      {/* {cuisine.map((recipes) => {
-        return <div key={recipes.id}>{recipes.title}</div>;
-      })} */}
-    </div>
+    <Grid>
+      {Cuisine?.map((item) => (
+        <Card key={item.id}>
+          <img src={item.image} alt={item.title} />
+          <h4>{item.title}</h4>
+        </Card>
+      ))}
+    </Grid>
   );
 }
 
-const Grid = styled.div``;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
 
 export default Cuisine;
